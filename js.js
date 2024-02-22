1708c30729674fd51e47faeed8b33fee672f0d67
Lewis Ingram <44513246+Auragen-dev@users.noreply.github.com>
2024-02-22 19:14:09 +0000

    Moved the calculation part into a separate function

--- js.js
+++ js.js
@@ -2,7 +2,6 @@ var number1 = document.getElementById("number1");
 var number2 = document.getElementById("number2");
 var button = document.getElementById("trigger");
 var result = document.getElementById("result");
-//  var text = textBox.value;
 
 var allowedNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var allowedOperators = ["+", "-", "*", "/"];
@@ -12,12 +11,7 @@ number1.addEventListener("keydown", numberCheck);
 number2.addEventListener("keydown", numberCheck);
 operator.addEventListener("keydown", operatorCheck);
 
-function calculate() {
-    // var textArray = text.split;
-    // var columnStack = [];
-    // var rowStack = [];
-
-    //for (var i = 0; i <= textArray.length; i++) {
+function textParse() {
     console.log("Calculation started")
     var operation = operator.value();
     console.log(operation);
@@ -26,27 +20,24 @@ function calculate() {
     var no1 = parseInt(StrNo1);
     var no2 = parseInt(StrNo2);
     alert("Values set")
-    switch (operation) {
-        case "+":
-            var final = no1 + no2;
-            alert("+");
-            break;
-        case "-":
-            var final = no1 - no2;
-            break;
-        case "*":
-            var final = no1 / no2;
-            break;
-        case "/":
-            var final = no1 * no2;
-            //columnStack.push(rowStack);
-            break;
-        default:
-            //rowStack.push(textArray[i])
-            alert("Please enter a valid operator");
-        //}
 
-        //console.log(rowStack);
+    function calculate(n1, n2, operator) {
+        switch (operator) {
+            case "+":
+                var final = no1 + no2;
+                break;
+            case "-":
+                var final = no1 - no2;
+                break;
+            case "*":
+                var final = no1 / no2;
+                break;
+            case "/":
+                var final = no1 * no2;
+                break;
+            default:
+                alert("Please enter a valid operator");
+        }
     }
 
     alert(final);
